eaccf94b68b3ab51f23e715594dac8f6c3d67cb3
Prajyot177 <1032201378@miwpu.edu.in>
2023-01-07 10:16:16 +0530

    feat: added routes, home, competitions page

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,19 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import NavbarComp from "./components/NavbarComp";
+import Home from "./components/Home";
+import Competitions from "./components/Competitions";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
+    <>
       <NavbarComp />
-    </div>
+      <Routes>
+        <Route path="/Home" element={<Home />}></Route>
+        <Route path="/Competitions" element={<Competitions />}></Route>
+      </Routes>
+    </>
   );
 }
 
--- client/src/components/NavbarComp.js
+++ client/src/components/NavbarComp.js
@@ -8,12 +8,12 @@ const NavbarComp = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="Home">React-Bootstrap</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link href="Home">Home</Nav.Link>
+            <Nav.Link href="Competitions">Competitions</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
