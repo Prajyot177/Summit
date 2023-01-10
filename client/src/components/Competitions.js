0e3b8352e1de10dba68db3d1af418b7eb6ee44c1
Prajyot177 <1032201378@miwpu.edu.in>
2023-01-10 12:13:11 +0530

    feat: integreten tailwind css

--- client/src/components/Competitions.js
+++ client/src/components/Competitions.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Competitions = () => {
-  return <div>Competitions</div>;
+  return (
+    <>
+      <div>Competitions</div>
+      <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+        <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
+        <p className="text-gray-500 text-lg">
+          React and Tailwind CSS in action
+        </p>
+      </div>
+    </>
+  );
 };
 
 export default Competitions;
